a98856f5cc0ad4566f544634ef3783414e07341d
mtaji <mtaji@taji.dev02>
2014-01-09 20:48:41 +0900

    Twitter投稿用のスクリプトを追加

---
+++ TweetBot.php
@@ -0,0 +1,46 @@
+<?php
+
+    require_once(dirname(__FILE__).'/twitteroauth/twitteroauth/twitteroauth.php');
+    require_once(dirname(__FILE__).'/lib/CSVReader.php');
+
+    // developer info
+    $consumerKey       = '';
+    $consumerSecret    = '';
+    $accessToken       = '';
+    $accessTokenSecret = '';
+
+    $oAuth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+    // oAuth認証を利用し、twitterに投稿する
+    $TWITTER_STATUS_UPDATE_URL = "http://api.twitter.com/1.1/statuses/update.json";
+    $method  = 'POST';
+    $message = getMessage();
+
+    var_dump($message);
+
+    $response = $oAuth->OAuthRequest($TWITTER_STATUS_UPDATE_URL, $method, array('status'=> $message));
+
+    // 結果出力
+    $result = json_decode($response);
+
+    print '<pre>';
+    var_dump($result);
+    print '</pre>';
+
+    /**
+     * 投稿メッセージを取得する
+     */
+    function getMessage(){
+
+        $reader = new CSVReader();
+
+        $csv = './lib/csv/test.csv';
+
+        $csv = $reader->read($csv);
+
+        $i = mt_rand(1, 4);
+
+        return (isset($csv[$i][1])) ? $csv[$i][1] : '';
+    }
+
+?>
